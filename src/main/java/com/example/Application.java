936d31d0783145913f46b361b3f1f78feec60eb5
weizujie <byojiaoxianz7@outlook.com>
2020-09-10 11:12:47 +0800

    部署上线

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,9 +1,30 @@
 package com.example;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 /**
  * @Author: weizujie
  * @Date: 2020/9/8
  * @Github: https://github.com/weizujie
  */
-public class Application {
+
+//@CrossOrigin
+//@MapperScan("com.example.mapper")
+//@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
 }
