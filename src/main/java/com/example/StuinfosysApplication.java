d05d492df8936ca6c0830bb37c855a64da3f9d03
weizujie <byojiaoxianz7@outlook.com>
2020-09-11 10:58:35 +0800

    启用HTTPS

--- src/main/java/com/example/StuinfosysApplication.java
+++ src/main/java/com/example/StuinfosysApplication.java
@@ -2,7 +2,6 @@ package com.example;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -14,5 +13,6 @@ public class StuinfosysApplication {
     public static void main(String[] args) {
         SpringApplication.run(StuinfosysApplication.class, args);
     }
+
 
 }
